d2793a4ab0b49464570c1ff23cccd47381414659
IcewineChen <xidianchr@163.com>
2018-08-21 16:44:46 +0800

    settings

--- utils/debugger.py
+++ utils/debugger.py
@@ -9,10 +9,6 @@ try:
     pass
 except:
     pass
-
-
-#def show3D(ax, points, c = (255, 0, 0), edges = []):
-
     
 def show2D(img, points, c):
     points = ((points.reshape(ref.nJoints, -1))).astype(np.int32)
@@ -49,37 +45,6 @@ class Debugger(object):
                                                      scale_factor=1)
         except:
             pass
-    
-    def addPoint3D(self, points, c = 'b', marker = 'o', edges = []):
-        #show3D(self.ax, point, c, marker = marker, edges)
-        points = points.reshape(-1, 3)
-        #print 'show3D', c, points
-        x, y, z = np.zeros((3, points.shape[0]))
-        for j in range(points.shape[0]):
-            x[j] = points[j, 0].copy()
-            y[j] = points[j, 1].copy()
-            z[j] = points[j, 2].copy()
-            self.xmax = max(x[j], self.xmax)
-            self.ymax = max(y[j], self.ymax)
-            self.zmax = max(z[j], self.zmax)
-            self.xmin = min(x[j], self.xmin)
-            self.ymin = min(y[j], self.ymin)
-            self.zmin = min(z[j], self.zmin)
-        if c == 'auto':
-            c = [(z[j] + 0.5, y[j] + 0.5, x[j] + 0.5) for j in range(points.shape[0])]
-        #print 'shape', x.shape, y.shape, z.shape, points.shape, c
-        self.ax.scatter(x, y, z, s = 200, c = c, marker = marker)
-        for e in edges:
-            self.ax.plot(x[e], y[e], z[e], c = c)
-        
-    def show3D(self):
-        max_range = np.array([self.xmax-self.xmin, self.ymax-self.ymin, self.zmax-self.zmin]).max()
-        Xb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten() + 0.5*(self.xmax+self.xmin)
-        Yb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten() + 0.5*(self.ymax+self.ymin)
-        Zb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten() + 0.5*(self.zmax+self.zmin)
-        for xb, yb, zb in zip(Xb, Yb, Zb):
-            self.ax.plot([xb], [yb], [zb], 'w')
-        self.plt.show()
         
     def addImg(self, img, imgId = 'default'):
         self.imgs[imgId] = img.copy()
@@ -109,15 +74,6 @@ class Debugger(object):
             cv2.imshow('{}'.format(i), v)
         if pause:
             cv2.waitKey()
-    
-    def save3D(self, path):
-        max_range = np.array([self.xmax-self.xmin, self.ymax-self.ymin, self.zmax-self.zmin]).max()
-        Xb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten() + 0.5*(self.xmax+self.xmin)
-        Yb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten() + 0.5*(self.ymax+self.ymin)
-        Zb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten() + 0.5*(self.zmax+self.zmin)
-        for xb, yb, zb in zip(Xb, Yb, Zb):
-            self.ax.plot([xb], [yb], [zb], 'w')
-        self.plt.savefig(path, bbox_inches='tight', frameon = False)
     
     def saveImg(self, imgId = 'default', path = '../debug/'):
         cv2.imwrite(path + '{}.png'.format(imgId), self.imgs[imgId])
